de5b749d712542f2699a61e6d8a9257c6bd9fc23
HoseaLeee <hosealee@126.com>
2018-04-24 19:46:47 +0800

    更改Demo

--- Example/Pods/Target Support Files/Pods-MGSocialShareKit_Example/Pods-MGSocialShareKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-MGSocialShareKit_Example/Pods-MGSocialShareKit_Example-frameworks.sh
@@ -143,10 +143,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/../../MGSocialShareKit/Classes/MGSocialShareKit.framework"
+  install_framework "${PODS_ROOT}/MGSocialShareKit/MGSocialShareKit/MGSocialShareKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/../../MGSocialShareKit/Classes/MGSocialShareKit.framework"
+  install_framework "${PODS_ROOT}/MGSocialShareKit/MGSocialShareKit/MGSocialShareKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
