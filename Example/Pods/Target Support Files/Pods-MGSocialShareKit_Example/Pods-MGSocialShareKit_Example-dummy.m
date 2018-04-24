343dc754c16e386593d6648f0337c9d5011a1861
HoseaLeee <hosealee@126.com>
2018-04-24 17:28:08 +0800

    重新提交

---
+++ Example/Pods/Target Support Files/Pods-MGSocialShareKit_Example/Pods-MGSocialShareKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MGSocialShareKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MGSocialShareKit_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-MGSocialShareKit_Tests/Pods-MGSocialShareKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MGSocialShareKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MGSocialShareKit_Tests
+@end
